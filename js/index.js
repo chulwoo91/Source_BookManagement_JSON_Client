7712e76b79457291864575696e3a61966c531fea
chulwoo91 <kinanimal@naver.com>
2016-11-07 17:27:57 +0900

    REALLY THE Final Project(Chulwoo Jeong, 정철우)

--- js/index.js
+++ js/index.js
@@ -219,11 +219,47 @@ function searchBook(){
 
 					$("tbody").append(tr);
 					};
+
+					$(document).ready(function(){
+
+						$('#nav').empty();
+
+						$('#data').after('<div id="nav"></div>');
+						var rowsShown = 20;
+						var rowsTotal = $('#myTbody tr').length;
+						var numPages = rowsTotal/rowsShown;
+
+						for(i = 0;i < numPages;i++) {
+							var pageNum = i + 1;
+							$('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+						}
+
+						$('#myTbody tr').hide();
+						$('#myTbody tr').slice(0, rowsShown).show();
+						$('#nav a:first').addClass('active');
+
+
+						$('#nav a').bind('click', function(){
+
+							$('#nav a').removeClass('active');
+							$(this).addClass('active');
+							var currPage = $(this).attr('rel');
+							var startItem = currPage * rowsShown;
+							var endItem = startItem + rowsShown;
+							$('#myTbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+							css('display','table-row').animate({opacity:1}, 300);
+
+						});
+					});
+
 				},
 				error: function(){
 					alert("there is something wrong");
 				}
 			});
+
+
+
 		}
 	}
 /*	function del(Object){
